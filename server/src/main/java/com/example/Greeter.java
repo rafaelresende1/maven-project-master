1c1e555d0448eb2dbd780d243ab863cd3b3bc627
rafaelresende1 <rafael.resende@outlook.com>
2018-04-12 21:49:11 -0300

    Initial commit

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+/*fd
+ * df
+ * df
+ */
+  public String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
